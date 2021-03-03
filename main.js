9bd202d126a14effe62f76cd3345547f6b8c6f2e
madalinagrama <m.madalinagrama@gmail.com>
2021-03-03 13:52:21 +0200

    win function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let statusDiv = document.querySelector('.status');
+const statusDiv = document.querySelector('.status');
 const cellDivs = document.querySelectorAll('.game-cell');
 const xSymbol = '×';
 const oSymbol = '○';
@@ -7,24 +7,48 @@ let xIsNext = true;
 const restartButton = document.getElementById('restartButton')
 const winningMessageElement = document.getElementById('winningMessage');
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const winners = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 const handleCellClick = (e) => {
     const classList = e.target.classList;
-    if (!gameIsLive || classList[1] === 'x'|| classList[1] === 'o'){
+    if (!gameIsLive || classList.contains('x') || classList.contains('o')){
         return;
     }
     if (xIsNext) {
         classList.add('x');
-        checkGameStatus();
+        xIsNext = false;
     } else {
         classList.add('o');
-        checkGameStatus();
+        xIsNext = true;
     }
+    checkGameStatus();
 };
 
 cellDivs.forEach(cell => {
     cell.addEventListener('click', handleCellClick);
 })
+
+const winFunction = () =>
+    any(
+        winners.map((c) => {
+            let row = cellDivs
+                .filter((_, i) => c.includes(i))
+                .map((d) => d.classList[1]);
+            let allX = all(row.map((r) => r === xSymbol));
+            let allO = all(row.map((r) => r === oSymbol));
+
+            return allX || allO;
+        })
+    );
 
 const checkGameStatus = () => {
   const topLeft = cellDivs[0].classList[1];
@@ -36,47 +60,33 @@ const checkGameStatus = () => {
   const bottomLeft = cellDivs[6].classList[1];
   const bottomMiddle = cellDivs[7].classList[1];
   const bottomRight = cellDivs[8].classList[1];
-
-  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-      handleWin(topLeft);
-      cellDivs[0].classList.add('won');
-      cellDivs[1].classList.add('won');
-      cellDivs[2].classList.add('won');
-  } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
-      handleWin(middleLeft);
-      cellDivs[3].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[5].classList.add('won');
-  } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-      handleWin(bottomLeft);
-      cellDivs[6].classList.add('won');
-      cellDivs[7].classList.add('won');
-      cellDivs[8].classList.add('won');
-  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
-      handleWin(topLeft);
-      cellDivs[0].classList.add('won');
-      cellDivs[3].classList.add('won');
-      cellDivs[6].classList.add('won');
-  } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
-      handleWin(topMiddle);
-      cellDivs[1].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[7].classList.add('won');
-  } else if (topRight && topRight === middleRight && topRight === bottomRight ) {
-      handleWin(topRight);
-      cellDivs[2].classList.add('won');
-      cellDivs[5].classList.add('won');
-      cellDivs[8].classList.add('won');
-  } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
-      handleWin(topLeft);
-      cellDivs[0].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[8].classList.add('won');
-  } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
-      handleWin(topRight);
-      cellDivs[2].classList.add('won');
-      cellDivs[4].classList.add('won');
-      cellDivs[6].classList.add('won');
+  let winningRow = [];
+  if (winFunction()){
+      return handleWin(xIsNext ? xSymbol : oSymbol);
+  // if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+  //     handleWin(topLeft);
+  //     winningRow = [0, 1, 2];
+  // } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+  //     handleWin(middleLeft);
+  //     winningRow = [ 3, 4, 5];
+  // } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+  //     handleWin(bottomLeft);
+  //     winningRow = [6, 7, 8];
+  // } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+  //     handleWin(topLeft);
+  //     winningRow = [0, 3, 6];
+  // } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+  //     handleWin(topMiddle);
+  //     winningRow = [1, 4, 7]
+  // } else if (topRight && topRight === middleRight && topRight === bottomRight ) {
+  //     handleWin(topRight);
+  //     winningRow = [5, 2, 8];
+  // } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+  //     handleWin(topLeft);
+  //     winningRow = [0, 4, 8];
+  // } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+  //     handleWin(topRight);
+  //     winningRow = [2, 4, 6];
   } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
       gameIsLive = false;
       winningMessageTextElement.innerText = 'Draw!';
@@ -89,6 +99,9 @@ const checkGameStatus = () => {
         statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
     }
   }
+  winningRow.forEach((e) => {
+      cellDivs[e].classList.add('won');
+  })
 };
 
 const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
